6514cf93f7723434678a326cc37a9a8557ff3dbf
vitonaryama <101534475+vitonaryama@users.noreply.github.com>
2025-12-30 21:01:12 +0700

    Add files via upload

---
+++ Guided/Guided/stack.h
@@ -0,0 +1,31 @@
+#ifndef STACK
+#define STACK
+#define Nil NULL
+
+#include <iostream>
+using namespace std;
+
+typedef struct node *address;
+
+struct node {
+    int dataAngka;
+    address next;
+};
+
+struct stack{
+    address top;
+};
+
+bool isEmpty(stack lisStack);
+void createStack(stack &listStack);
+address alokasi(int angka);
+void dealokasi(address &node);
+
+void push(stack &listStack, address nodeBaru); 
+void pop(stack &listStack);
+void update(stack &listStack, int posisi);
+void view(stack listStack);
+void searchData(stack listStack, int data);
+
+#endif
+
